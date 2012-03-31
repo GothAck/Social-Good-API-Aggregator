215d3f0ed7f55c816ba4de01aa64bac37de6a01e
Greg Miell <greg@greg-net.co.uk>
2012-03-31 14:10:03 +0100

    Added info to site, fixed up about pages with nav

--- app/controllers/about_controller.js
+++ app/controllers/about_controller.js
@@ -4,17 +4,21 @@ var locomotive = require('locomotive')
 var AboutController = new Controller();
 
 var pages = {
-  contact: 'Contact'
+  contact: 'Contact',
+  charity: 'Charity',
+  agency: 'Agency',
+  apps: 'Apps'
 }
 
 AboutController.index = function() {
   this.title = 'About';
+  this.page = null;
   console.log('index', this.req);
   this.render(null, {layout : ( (this.param('format') === 'frag') ? null : undefined ) });
 }
 
 AboutController.page = function() {
-  var page = this.param('page');
+  var page = this.page = this.param('page');
   console.log(this);
   if (!(page in pages)) return this.res.send(404);
   this.title = 'Contact';
